16658b454ce484c0e49d08db59f274536b954d8e
Dan <daniel.speranskiy@live.com>
2023-11-19 18:43:21 +0400

    Adding in the home page to display user gen pics

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
+import { download } from '../assets'
+import { downloadImage } from '../utils'
+
 function Card() {
   return (
-    <div>Card</div>
+    <div>
+
+    </div>
   )
 }
 
